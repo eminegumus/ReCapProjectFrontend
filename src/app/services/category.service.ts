53186209e1f157dbceb1d8ccc2a2f065ce52d39b
İsmail Gümüş <i.gumusmadenoglu@gmail.com>
2021-03-17 23:50:34 +0300

    18.ders ortası

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -2,18 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/ListResponseModel';
-import { Product } from '../models/product';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class CategoryService {
 
-  apiUrl="https://localhost:44300/api/products/getall";
+  apiUrl="https://localhost:44300/api/categories/getall";
   constructor(private httpClient:HttpClient) {}
   
-  getProducts():Observable<ListResponseModel<Product>>{
+  getCategories():Observable<ListResponseModel<Category>>{
     return this.httpClient
-    .get<ListResponseModel<Product>>(this.apiUrl);
+    .get<ListResponseModel<Category>>(this.apiUrl);
   }
 }

60bdba4164fc8920758f824818284451d2f68baf
İsmail Gümüş <i.gumusmadenoglu@gmail.com>
2021-03-22 22:37:29 +0300

    20.gün ders sonu

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -30,21 +30,23 @@ export class ProductAddComponent implements OnInit {
   }
 
   add(){
-    if (this.productAddForm.valid) {
-      let productModel=Object.assign({},this.productAddForm.value);
+    if(this.productAddForm.valid){
+      let productModel = Object.assign({},this.productAddForm.value)
       this.productService.add(productModel).subscribe(response=>{
-        console.log(response);
         this.toastrService.success(response.message,"Başarılı")
       },responseError=>{
-        console.log(responseError.error)
-      });
+        if(responseError.error.Errors.length>0){
+          for (let i = 0; i <responseError.error.Errors.length; i++) {
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage
+              ,"Doğrulama hatası")
+          }       
+        } 
+      })
       
     }else{
-      this.toastrService.error("Formunuz eksik","Dikkat");
-      
+      this.toastrService.error("Formunuz eksik","Dikkat")
     }
-   
-   
+    
   }
 
-}
+} 
